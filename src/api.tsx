ab3aa5e59acd2640e29b126d322b70019e4523a9
meluvrose <tanggu3910@gmail.com>
2023-03-07 03:37:58 +0900

    #9.6 Home Screen part 2

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,25 @@
 const API_KEY = "312101e39e8e4e33e106a818028fdcd9";
 const BASE_URL = "https://api.themoviedb.org/3";
 
+interface IMovies {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovies[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
